f19ec29d669f83c1a125527d1c2d4368bc5c6fe3
kamilW <kamil.wisniewski@edu.uekat.pl>
2019-09-10 20:07:07 +0200

    finder init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "",
+    status: ""
+  }
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.name}/`);
+    console.log(await res.json());
+
+  }
+  handleChange = async (e) => {
+    console.log(e.target.value)
+    this.setState({ name: e.target.value })
+    let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.name}/`);
+    if (res.status !== 200) {
+      this.setState({ state: false });
+      console.log("blad");
+    } else {
+      console.log(res);
+    }
+  }
+  renderLoader = () => {
+    if (!this.state.status) {
+      return (
+        <div class="spinner-border" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      )
+    } else {
+
+    }
+  }
+  async componentDidMount() {
+    let res = await fetch('https://pokeapi.co/api/v2/pokemon/?&limit=400')
+    let data = await res.json();
+    const names = data.results.map(ele => ele.name);
+    const val = 'ab'
+    const regex = new RegExp(`^${val}`, "g");
+    console.log(regex)
+    const filtr = names.filter(name => {
+      if (name.match(regex)) {
+        return true
+      }
+      else return false
+    })
+    console.log(filtr);
+  }
+  render() {
+    return (
+      <div className="App" >
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="pokemonName">Pokemon name:</label>
+              <p>{this.state.name}</p>
+              <input className="mb-3 form-control" id="pokemonName" type="text" onChange={this.handleChange} />
+              <input className="btn btn-primary" type="submit" value="get Pokemon" ></input>
+            </div>
+          </form>
+        </div>
+        {this.renderLoader()}
+      </div >
+    );
+  }
 }
 
 export default App;
